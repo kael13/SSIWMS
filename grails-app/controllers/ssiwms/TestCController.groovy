792a745e6b1a65879ce455b5609f0e9da9f0bbaa
reinduqueii <reinduque@microwurx.com>
2014-11-19 12:13:26 +0800

    Added Comment 2

--- grails-app/controllers/ssiwms/TestCController.groovy
+++ grails-app/controllers/ssiwms/TestCController.groovy
@@ -5,6 +5,6 @@ class TestCController {
     def index() {
     render("Hello World")
         render("What's up")
-         render("What's up") //Rein's Comment
+         render("What's up") //Rein's Comment 2
     }
 }
