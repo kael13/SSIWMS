53b99e02a6807d8bcfcd5f9cc38e4657845df7d3
reynaflorsentillas <reynaflorsentillas@ymail.com>
2014-11-20 09:23:08 -0800

    Rein2 Test Today

--- grails-app/controllers/ssiwms/TestCController.groovy
+++ grails-app/controllers/ssiwms/TestCController.groovy
@@ -3,9 +3,10 @@ package ssiwms
 class TestCController {
 
     def index() {
-    render("Hello World")
+        render("Hello World")
         render("What's up")
-         render("What's up") //Rein's Comment 2
+        render("What's up") //Rein's Comment 2
         //Test
+        render("This is my line")
     }
 }
