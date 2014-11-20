735cb189e9498e8255b8b3dba35f3dab532a828f
reynaflorsentillas <reynaflorsentillas@ymail.com>
2014-11-20 09:24:46 -0800

    Rein2 Add Comment

--- grails-app/controllers/ssiwms/TestCController.groovy
+++ grails-app/controllers/ssiwms/TestCController.groovy
@@ -7,6 +7,6 @@ class TestCController {
         render("What's up")
         render("What's up") //Rein's Comment 2
         //Test
-        render("This is my line")
+        render("This is my line") //Rein 2 Comment
     }
 }
